796c5727f2d3d20dc81bb79c5689744aa2f4db30
fay <869770665@qq.com>
2021-01-08 15:16:01 +0800

    upd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-// import Provider from '@/app/provider';
-// import {PATH_PREFIX} from "@/env";
-// import '../styles/global.css';
-// import 'swiper/swiper-bundle.css';
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <React.Fragment>
       <Head>
-        <title>FilBox</title>
+        <title>@fay-next/config</title>
         <meta name="keywords" content="DataBrowser"/>
         <meta name="description" content="DataBrowser"/>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
         <meta name="renderer" content="webkit"/>
-        {/* <link rel="manifest" href={`${PATH_PREFIX}/manifest.json`} /> */}
-        {/* <link rel="icon" type="image/x-icon" href={`${PATH_PREFIX}/favicon.ico`} /> */}
         <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,minimal-ui"/>
-        {/* <script src={`${PATH_PREFIX}/lib/judge-terminal-equipment.js`}/> */}
       </Head>
-      {/* <Provider> */}
-        <Component {...pageProps} />
-      {/* </Provider> */}
+      <Component {...pageProps} />
     </React.Fragment>
   );
 }
